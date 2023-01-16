935705cf09216a47ce1e6debc75a998c06c72b31
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-16 10:04:31 -0300

    add limites

--- busqueda.py
+++ busqueda.py
@@ -12,3 +12,11 @@ def busqueda_ingenua(lista, objetivo):
 
 miLista = [1,2,3,4,5,10,12]
 print(busqueda_ingenua(miLista, 10))
+
+def busqueda_binaria(lista, objetivo, limite_inferior=None, limite_superior=None):
+    if limite_inferior is None:
+        limite_inferior = 0 # inicio de la lista
+    if limite_superior is None:
+        limite_superior = len(lista)-1 # fin de la lista 
+
+    
